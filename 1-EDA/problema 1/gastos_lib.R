15a88f306d528e02fc1c21b91da60c4598740791
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2016-07-08 15:23:24 -0300

    Ambos os ler_gastos retornam datas

--- 1-EDA/problema 1/gastos_lib.R
+++ 1-EDA/problema 1/gastos_lib.R
@@ -2,14 +2,14 @@ ler_gastos <- function(arquivo = "../dados/ano-atual.csv"){
   #' Lê um csv criado a partir dos dados de gastos dos deputados da 
   #' Câmara e seta os tipos de colunas mais convenientemente. 
   require("readr")
+  require("lubridate")
   require("dplyr", warn.conflicts = FALSE)
   
-  gastos = read_csv(arquivo, 
-                    col_types = list(
-                      datEmissao = col_datetime()
-                    ))
+  gastos = read_csv(arquivo)
   gastos = gastos %>% 
-    mutate_each(funs(as.factor), sgPartido, sgUF, txNomeParlamentar, indTipoDocumento)
+    mutate_each(funs(as.factor), sgPartido, sgUF, txNomeParlamentar, indTipoDocumento) %>% 
+    mutate(datEmissao = ymd(substr(datEmissao, 1, 10)))
+  
   return(gastos)
 } 
 
@@ -19,9 +19,11 @@ ler_gastos2 <- function(arquivo = "../dados/ano-atual.csv"){
   #' Versão sem readr, para máquinas onde não é possível instalar esse pacote. 
   #' É um pouco mais lenta que a com readr.
   require("dplyr", warn.conflicts = FALSE)
+  require("lubridate")
   
   gastos = read.csv(arquivo, stringsAsFactors = FALSE)
   gastos = gastos %>% 
     mutate_each(funs(as.factor), sgPartido, sgUF, txNomeParlamentar, indTipoDocumento)
+  gastos$datEmissao = ymd(substr(gastos$datEmissao, 1, 10))
   return(gastos)
 } 
