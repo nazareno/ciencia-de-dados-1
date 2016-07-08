49512fbda89b182c545dde9342aa2baae34fd621
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2016-07-08 14:49:18 -0300

    Cria versão de ler_gastos sem readr

--- 1-EDA/problema 1/gastos_lib.R
+++ 1-EDA/problema 1/gastos_lib.R
@@ -12,3 +12,16 @@ ler_gastos <- function(arquivo = "../dados/ano-atual.csv"){
     mutate_each(funs(as.factor), sgPartido, sgUF, txNomeParlamentar, indTipoDocumento)
   return(gastos)
 } 
+
+ler_gastos2 <- function(arquivo = "../dados/ano-atual.csv"){
+  #' Lê um csv criado a partir dos dados de gastos dos deputados da 
+  #' Câmara e seta os tipos de colunas mais convenientemente. 
+  #' Versão sem readr, para máquinas onde não é possível instalar esse pacote. 
+  #' É um pouco mais lenta que a com readr.
+  require("dplyr", warn.conflicts = FALSE)
+  
+  gastos = read.csv(arquivo, stringsAsFactors = FALSE)
+  gastos = gastos %>% 
+    mutate_each(funs(as.factor), sgPartido, sgUF, txNomeParlamentar, indTipoDocumento)
+  return(gastos)
+} 
