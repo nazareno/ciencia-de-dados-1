e944b03739495f0fe2425da478068c0feb8dd643
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2016-07-08 14:42:53 -0300

    Lê datas como datas

--- 1-EDA/problema 1/gastos_lib.R
+++ 1-EDA/problema 1/gastos_lib.R
@@ -1,9 +1,12 @@
 ler_gastos <- function(arquivo = "../dados/ano-atual.csv"){
+  #' Lê um csv criado a partir dos dados de gastos dos deputados da 
+  #' Câmara e seta os tipos de colunas mais convenientemente. 
   require("readr")
   require("dplyr", warn.conflicts = FALSE)
   
   gastos = read_csv(arquivo, 
                     col_types = list(
+                      datEmissao = col_datetime()
                     ))
   gastos = gastos %>% 
     mutate_each(funs(as.factor), sgPartido, sgUF, txNomeParlamentar, indTipoDocumento)
