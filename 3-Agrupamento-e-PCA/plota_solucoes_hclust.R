a9ec0998266b366249e6f7e97fc7a087b86573b5
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2018-06-10 18:34:02 -0300

    Refatora funções auxiliares para vis de grupos

--- 3-Agrupamento-e-PCA/plota_solucoes_hclust.R
+++ 3-Agrupamento-e-PCA/plota_solucoes_hclust.R
@@ -1,3 +1,12 @@
+get_grupos <- function(agrupamento, num_grupos){
+    agrupamento %>% 
+        cutree(num_grupos) %>% 
+        as.data.frame() %>% 
+        mutate(label = rownames(.)) %>% 
+        gather(key =  "k", value = "grupo", -label) %>% 
+        mutate(grupo = as.character(grupo))
+}
+
 plota_hclusts_1d = function(dados_filme, 
                             nome_coluna, # coluna usada para distâncias
                             dist_method = "euclidean", 
@@ -14,17 +23,19 @@ plota_hclusts_1d = function(dados_filme,
         dist(method = dist_method) %>% 
         hclust(method = linkage_method)
     
-    atribuicoes = tibble(k = ks) %>% 
-        group_by(k) %>% 
-        do(cbind(filmes, 
-                 grupo = as.character(cutree(agrupamento_h, .$k)))) 
+    atribuicoes = get_grupos(agrupamento_h, num_grupos = ks)
+    
+    atribuicoes = atribuicoes %>% 
+        mutate(TITLE = label) %>% 
+        left_join(filmes, by = "TITLE")
+    
     atribuicoes %>% 
         ggplot(aes_string(y = nome_coluna, colour = "grupo")) + 
         geom_jitter(aes(x = " "), 
-                    width = .02, height = 0, size = 2, alpha = .6) + 
+                    width = .02, height = 0, size = 1.6, alpha = .6) + 
         facet_wrap(~ paste(k, " grupos")) + 
-        xlab("") %>% 
-        return()
+        scale_color_brewer(palette = "Set2") + 
+        xlab("") 
 }
 
 plota_hclusts_2d = function(agrupamento,
@@ -38,15 +49,15 @@ plota_hclusts_2d = function(agrupamento,
     library(ggplot2)
     library(dplyr, warn.conflicts = F)
     
-    atribuicoes = tibble(k = ks) %>% 
-        group_by(k) %>% 
-        do(cbind(filmes, 
-                 grupo = as.character(cutree(agrupamento, .$k)))) 
+    atribuicoes = get_grupos(agrupamento, num_grupos = ks)
+    
+    atribuicoes = atribuicoes %>% 
+        mutate(TITLE = label) %>% 
+        left_join(filmes, by = "TITLE")
     
     atribuicoes %>% 
         ggplot(aes_string(x = nome_colunas[1], y = nome_colunas[2], colour = "grupo")) + 
-        geom_jitter(width = .02, height = 0, size = 2, alpha = .6) + 
+        geom_jitter(width = .02, height = 0, size = 1.6, alpha = .6) + 
         facet_wrap(~ paste(k, " grupos")) + 
-        xlab("") %>% 
-        return()
+        scale_color_brewer(palette = "Set2")
 }
