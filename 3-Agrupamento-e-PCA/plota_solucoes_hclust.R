3eae241c264d04898fb1308114bdfeb2229d66a0
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2018-06-17 20:44:08 -0300

    Refatora

--- 3-Agrupamento-e-PCA/plota_solucoes_hclust.R
+++ 3-Agrupamento-e-PCA/plota_solucoes_hclust.R
@@ -57,7 +57,32 @@ plota_hclusts_2d = function(agrupamento,
     
     atribuicoes %>% 
         ggplot(aes_string(x = nome_colunas[1], y = nome_colunas[2], colour = "grupo")) + 
-        geom_jitter(width = .02, height = 0, size = 1.6, alpha = .6) + 
+        geom_jitter(width = .02, height = 0, size = 2.6, alpha = .6) + 
+        facet_wrap(~ paste(k, " grupos")) + 
+        scale_color_brewer(palette = "Set2")
+}
+
+
+plota_hclusts_md = function(agrupamento,
+                            dados_filme,
+                            nome_colunas, # coluna usada para distâncias
+                            dist_method = "euclidean", 
+                            linkage_method = "complete", 
+                            ks = 1:9){
+    #' Retorna um ggplot das soluções de agrupamento de `dados_filme` 
+    #' para as quantidades de grupos em `ks` usando `hclust`.
+    library(ggplot2)
+    library(dplyr, warn.conflicts = F)
+    
+    atribuicoes = get_grupos(agrupamento, num_grupos = ks)
+    
+    atribuicoes = atribuicoes %>% 
+        mutate(TITLE = label) %>% 
+        left_join(filmes, by = "TITLE")
+    
+    atribuicoes %>% 
+        ggplot(aes_string(x = nome_colunas[1], y = nome_colunas[2], colour = "grupo")) + 
+        geom_jitter(width = .02, height = 0, size = 2.6, alpha = .6) + 
         facet_wrap(~ paste(k, " grupos")) + 
         scale_color_brewer(palette = "Set2")
 }
